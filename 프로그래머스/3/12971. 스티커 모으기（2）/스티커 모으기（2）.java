ed11a89bdbe32d3c0a0f828f65ec4c013eb5365e
kimhyeongjun-1204 <kl1533@gachon.ac.kr>
2026-01-22 13:02:51 +0900

    [level 3] Title: 스티커 모으기(2), Time: 11.32 ms, Memory: 60 MB -BaekjoonHub

---
+++ 프로그래머스/3/12971. 스티커 모으기（2）/스티커 모으기（2）.java
@@ -0,0 +1,32 @@
+import java.lang.Math; 
+
+class Solution {
+    public int solution(int sticker[]) {
+        int answer = 0;
+
+        int len = sticker.length;
+        int[] dp1 = new int[len];
+        int[] dp2 = new int[len];   
+        
+        // dp1 => 0을 무조건 선택 , dp2 => 0을 무조건 선택 X 
+        dp1[0] = sticker[0];
+        dp2[0] = 0;
+        
+        if(len > 1) {
+            dp1[1] = sticker[0];
+            dp2[1] = sticker[1];    
+        }
+        
+        
+        for(int i=2;i<len-1;i++) {
+            dp1[i] = Math.max(dp1[i-1], dp1[i-2] + sticker[i]);
+        }
+
+        for(int i=2;i<len;i++) {
+            dp2[i] = Math.max(dp2[i-1], dp2[i-2] + sticker[i]);
+        }
+        
+        
+        return len > 1 ? Math.max(dp1[len-2] , dp2[len-1]) : sticker[0];
+    }
+}
